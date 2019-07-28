a991e942c27fafa68a3b9fdef83e551154e273c7
ShadowLight8 <stuff@nickandbeth.com>
2019-07-27 21:16:47 -0400

    Updating scripts with 2018 versions

--- Pi-Temp-Check/check_temp.sh
+++ Pi-Temp-Check/check_temp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
 cpu=$(</sys/class/thermal/thermal_zone0/temp)
 gpu=$(/opt/vc/bin/vcgencmd measure_temp)
 echo "CPU @ $(echo 'scale=1;'$cpu/1000 | bc)"$'\xc2\xb0'"C"
 echo "GPU @ ${gpu:5:-2}"$'\xc2\xb0'"C"
+vcgencmd measure_clock arm | awk -F"=" '{printf ("CPU Freq @ %0.0f MHz\n",$2/1000000); }'
