abf71f08acace157a248d10e2e844be7b34aa8b4
ShadowLight8 <stuff@nickandbeth.com>
2023-11-11 22:57:38 -0500

    Projector and Countdown updates

--- Projector Controls/proj-on.sh
+++ Projector Controls/proj-on.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo stty -F /dev/ttyUSB0 9600 raw -echo -echoe -echok -echoctl -echoke
-echo -e -n '\r' > /dev/ttyUSB0
+sudo echo -e -n '\r' > /dev/ttyUSB0
 sleep 1
-echo -e -n '*pow=on#\r' > /dev/ttyUSB0
+sudo echo -e -n '*pow=on#\r' > /dev/ttyUSB0
